466280576bdf4668735b146ebabbeecb6bf879d2
Santiago Garcia <santiagogarcia@uninorte.edu.co>
2025-06-17 10:01:09 -0500

    GallonsAutomaticCalcullated
    Automatic Gallons when amount is writed

--- APP.Eds/APP.Eds/Components/PopUp/AddDispenser.xaml.cs
+++ APP.Eds/APP.Eds/Components/PopUp/AddDispenser.xaml.cs
@@ -3,17 +3,28 @@ using CommunityToolkit.Maui.Views;
 
 namespace APP.Eds.Components.PopUp;
 
+
 public partial class AddDispenser : Popup
 {
     private readonly CourtService courtService;
+    private bool isGallonsEditable = false;
 
     public AddDispenser(CourtService courtService)
     {
         InitializeComponent();
         this.courtService = courtService;
-
+        SecondEntry.IsEnabled = isGallonsEditable;
     }
-
+    private void EditGallonsButton_Clicked(object sender, EventArgs e)
+    {
+        isGallonsEditable = !isGallonsEditable;
+        SecondEntry.IsEnabled = isGallonsEditable;
+        if (isGallonsEditable)
+        {
+            SecondEntry.Focus();
+            SecondEntry.CursorPosition = SecondEntry.Text?.Length ?? 0;
+        }
+    }
     private void OnCloseTapped(object sender, EventArgs e)
     {
         if (BindingContext is CourtService vm)
@@ -73,26 +84,31 @@ public partial class AddDispenser : Popup
             await Application.Current.MainPage.DisplayAlert("Error", "Por favor, seleccione una Manguera", "OK");
         }
     }
-    
-    private void OnEntryUnfocused(object sender, FocusEventArgs e)
-    {
-        if (BindingContext is CourtService vm)
-        {
-            AmountBoxView.Color = vm.AccumulatedAmount >= vm.LastAccumulatedAmount ? Colors.Green : Colors.Red;
-            GallonBoxView.Color = vm.AccumulatedGallons >= vm.LastAccumulatedGallons ? Colors.Green : Colors.Red;
-        }
-    }
+    private const double PricePerGallon = 5000.0;
     private void EntryAccumulatedCompleted(object sender, EventArgs e)
     {
         if (BindingContext is CourtService vm)
         {
             AmountBoxView.Color = vm.AccumulatedAmount >= vm.LastAccumulatedAmount ? Colors.Green : Colors.Red;
             GallonBoxView.Color = vm.AccumulatedGallons >= vm.LastAccumulatedGallons ? Colors.Green : Colors.Red;
+
+            // Cálculo automático de galones
+            if (vm.AccumulatedAmount > vm.LastAccumulatedAmount)
+            {
+                double diferenciaMonto = vm.AccumulatedAmount - vm.LastAccumulatedAmount;
+                vm.AccumulatedGallons = vm.LastAccumulatedGallons + (diferenciaMonto / PricePerGallon);
+            }
+
             SecondEntry.Focus();
             SecondEntry.CursorPosition = SecondEntry.Text.Length;
-
         }
     }
+   
+    private void OnEntryUnfocused(object sender, FocusEventArgs e)
+    {
+
+    }
+
 
     private void EntryGallonsCompleted(object sender, EventArgs e)
     {
