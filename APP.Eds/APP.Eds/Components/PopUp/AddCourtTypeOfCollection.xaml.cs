9bc46f52ba21663f58f97e6451c580a73e3ababf
JOSE NAVARRO <janavarro724@gmail.com>
2025-06-24 09:07:15 -0500

    Ok Validation App

--- APP.Eds/APP.Eds/Components/PopUp/AddCourtTypeOfCollection.xaml.cs
+++ APP.Eds/APP.Eds/Components/PopUp/AddCourtTypeOfCollection.xaml.cs
@@ -36,18 +36,13 @@ public partial class AddCourtTypeOfCollection : Popup
             await Application.Current.MainPage.DisplayAlert("Error", "Por favor, el egreso debe ser mayor a 0", "OK");
             return;
         }
-        //if (string.IsNullOrWhiteSpace(courtService.CourtTypeOfCollectionDescription))
-        //{
-        //    await Application.Current.MainPage.DisplayAlert("Error", "Por favor, ingrese una descripcion", "OK");
-        //    return;
-        //}
         await courtService.AddCourtTypeOfCollectionFromPopup();
 
         TypeOfCollentionPicker.SelectedItem = null;
         FirstEntry.IsEnabled = false;
         SecondEntry.IsEnabled = false;
 
-        //Close();
+        Close();
     }
 
     private void TypeOfCollentionSelected(object sender, EventArgs e)
