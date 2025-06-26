25fe305756f198a532fe30ed22252d6b958b2b35
Sebastian1401 <147678097+Sebastian1401@users.noreply.github.com>
2025-06-26 11:21:15 -0500

    Unify Shopping

--- APP.Eds/APP.Eds/Models/Compartiment/CompartimentResponse.cs
+++ APP.Eds/APP.Eds/Models/Compartiment/CompartimentResponse.cs
@@ -12,16 +12,16 @@ public class CompartimentResponse
     public int Number { get; set; }
 
     [JsonPropertyName("nominal")]
-    public int Nominal { get; set; }
+    public double Nominal { get; set; }
 
     [JsonPropertyName("operative")]
-    public int Operative { get; set; }
+    public double Operative { get; set; }
 
     [JsonPropertyName("stock")]
-    public int Stock { get; set; }
+    public double Stock { get; set; }
 
     [JsonPropertyName("height")]
-    public int Height { get; set; }
+    public double Height { get; set; }
 
     [JsonPropertyName("idTank")]
     public int IdTank { get; set; }
--- APP.Eds/APP.Eds/Models/Shopping/ShoppingModel.cs
+++ APP.Eds/APP.Eds/Models/Shopping/ShoppingModel.cs
@@ -7,5 +7,13 @@ public class ShoppingModel
     public double Amount { get; set; }
     public int IdProvider { get; set; }
     public int IdCategory { get; set; }
+    public List<ShoppingProductNestedModel> ShoppingProducts { get; set; } = new();
+}
 
+public class ShoppingProductNestedModel
+{
+    public int IdProduct { get; set; }
+    public double Quantity { get; set; }
+    public double Price { get; set; }
+    public int IdCompartment { get; set; }
 }
