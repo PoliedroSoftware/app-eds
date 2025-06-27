8cc0716eccec91b6a4b1aea21b496dfbd8b5bafa
Dilan Vega <wissendeveloper@gmail.com>
2025-06-26 22:01:04 -0500

    Fixed Placeholders and translations

--- APP.Eds/APP.Eds/Models/Capacity/CapacityModel.cs
+++ APP.Eds/APP.Eds/Models/Capacity/CapacityModel.cs
@@ -2,9 +2,9 @@
 
 public class CapacityModel
 {
-    public int IdCapacity { get; set; }
-    public string Code { get; set; } = string.Empty;
-    public double Height { get; set; } = 0;
-    public double Gallon { get; set; } = 0;
-    public int Liters { get; set; } = 0;
+    public int? IdCapacity { get; set; }
+    public string? Code { get; set; } = string.Empty;
+    public double? Height { get; set; } = 0;
+    public double? Gallon { get; set; } = 0;
+    public int? Liters { get; set; } = 0;
 }
