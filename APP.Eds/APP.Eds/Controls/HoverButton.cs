b15285e7d75b04932d87faa0b56d3a44125649d2
William0814 <king-master90@hotmail.com>
2025-07-09 22:58:30 +0200

    Add solution of BUG 1007

--- APP.Eds/APP.Eds/Controls/HoverButton.cs
+++ APP.Eds/APP.Eds/Controls/HoverButton.cs
@@ -16,7 +16,6 @@ namespace APP.Eds.Controls
         public Color HoverColor { get; set; } = Colors.DarkGreen;
         public Color PressedColor { get; set; } = Colors.White;
 
-
         public HoverButton()
         {
             BackgroundColor = NormalColor;
@@ -74,6 +73,74 @@ namespace APP.Eds.Controls
 
         private Color InterpolateColor(Color from, Color to, double t)
         {
+
+            return Color.FromRgba(
+                from.Red + (to.Red - from.Red) * t,
+                from.Green + (to.Green - from.Green) * t,
+                from.Blue + (to.Blue - from.Blue) * t,
+                from.Alpha + (to.Alpha - from.Alpha) * t);
+        }
+    }
+
+    public class HoverButtonPopup : MauiButton
+    {
+        public Color NormalColorPopup { get; set; } = Colors.Gray;
+        public Color PressedColorPopup { get; set; } = Colors.Black;
+        public Color HoverColorPopup { get; set; } = Colors.Black;
+        public HoverButtonPopup()
+        {
+            BackgroundColor = NormalColorPopup;
+
+            Pressed += OnPointerEntered;
+            Released += OnReleasedPopup;
+
+            var pointerGesture = new PointerGestureRecognizer();
+            pointerGesture.PointerEntered += OnPointerEntered;
+            pointerGesture.PointerExited += OnPointerExited;
+            GestureRecognizers.Add(pointerGesture);
+        }
+
+        private async void OnReleasedPopup(object sender, EventArgs e)
+        {
+            _ = AnimateColorAsync(NormalColorPopup);
+        }
+
+        private async void OnPressedPopup(object sender, EventArgs e)
+        {
+            _ = AnimateColorAsync(PressedColorPopup);
+        }
+
+        private async void OnPointerEntered(object sender, EventArgs e)
+        {
+            _ = AnimateColorAsync(HoverColorPopup);
+        }
+
+        private async void OnPointerExited(object sender, EventArgs e)
+        {
+            _ = AnimateColorAsync(NormalColorPopup);
+        }
+
+        private async Task AnimateColorAsync(Color targetColor)
+        {
+            var startColor = BackgroundColor;
+
+            const int steps = 10;
+            const int totalDuration = 150; // ms
+            int delay = totalDuration / steps;
+
+            for (int i = 1; i <= steps; i++)
+            {
+                var interpolated = InterpolateColor(startColor, targetColor, (double)i / steps);
+                BackgroundColor = interpolated;
+                await Task.Delay(delay);
+            }
+        }
+
+        private Color InterpolateColor(Color from, Color to, double t)
+        {
+            if (from.IsDefault()) from = Colors.Transparent;
+            if (to.IsDefault()) to = Colors.Transparent;
+
             return Color.FromRgba(
                 from.Red + (to.Red - from.Red) * t,
                 from.Green + (to.Green - from.Green) * t,
