0f32c02b3c888bf638e6cdfb0fcecde985f974ad
JOSE NAVARRO <janavarro724@gmail.com>
2025-07-15 15:58:34 -0500

    Ok Bugs Solution

--- APP.Eds/APP.Eds/Services/Navigation/MainService.cs
+++ APP.Eds/APP.Eds/Services/Navigation/MainService.cs
@@ -60,32 +60,32 @@ namespace APP.Eds.Services.Navigation
                 {
                     new("Capacidad", typeof(CapacityPostView)),
                     new("Compartimento", typeof(CompartimentPostView)),
-                    new("Capacidad del compartimento", typeof(CompartimentCapacityPostView)),
-                    new("EdsTanque", typeof(EdsTankPostView)),
+                    new("Capacidad Del Compartimento", typeof(CompartimentCapacityPostView)),
+                    new("Tanque EDS", typeof(EdsTankPostView)),
                     new("Tanque", typeof(TankPostView)),
-                    new("Compartimento del producto", typeof(ProductCompartimentPostView))
+                    new("Compartimento Del Producto", typeof(ProductCompartimentPostView))
                 }),
                 new("Dispensadores y Mangueras", new List<MenuItemModel>
                 {
                     new("Dispensadores", typeof(DispensersPostView)),
-                    new("Tipo de dispensador", typeof(DispenserTypePostView)),
+                    new("Tipo De Dispensador", typeof(DispenserTypePostView)),
                     new("Manguera", typeof(HosePostView)),
-                    new("Historial de la manguera", typeof(HoseHistoryPostView))
+                    new("Historial De La Manguera", typeof(HoseHistoryPostView))
                 }),
                 new("Productos y Compras", new List<MenuItemModel>
                 {
-                    new("producto", typeof(ProductPostView)),
-                    new("Tipo de producto", typeof(ProductTypePostView)),
+                    new("Producto", typeof(ProductPostView)),
+                    new("Tipo De P1roducto", typeof(ProductTypePostView)),
                     new("Compras", typeof(ShoppingPostView)),
                 }),
                 new("EDS y Otros", new List<MenuItemModel>
                 {
-                    new("Eds", typeof(EdsPostView)),
-                    new("Gasto", typeof(ExpendituresPostView)),
-                    new("Islero", typeof(IslanderPostView)),
+                    new("Registre Una EDS", typeof(EdsPostView)),
+                    new("Tipo De Gastos", typeof(ExpendituresPostView)),
+                    new("Registre Un Islero", typeof(IslanderPostView)),
                      new("Isla", typeof(IslandPostView)),
                     new("Categoría", typeof(CategoryPostView)),
-                    new("Tipo de colección", typeof(TypeOfCollectionPostView))
+                    new("Tipo De Colección", typeof(TypeOfCollectionPostView))
                 }),
                  new("Inventario",
                 [
