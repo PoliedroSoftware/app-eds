63d8c81fc82902271d8744a6f3a06a7792b81117
William0814 <king-master90@hotmail.com>
2025-07-22 18:38:12 +0200

    Add more changes to testing

---
+++ APP.Eds/Helpers/Drivers/Mobile/AndroidDriverBuilder.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Android;
+
+namespace Helpers.Drivers.Mobile
+{
+    public static class AndroidDriverBuilder
+    {
+        public static AppiumDriver GetDriver()
+        {
+            var options = new AppiumOptions();
+
+            options.PlatformName = Configuration.PlatfromName;
+            options.AutomationName = "UiAutomator2";
+            options.AddAdditionalAppiumOption("appium:appPackage", Configuration.AppPackage);
+            options.AddAdditionalAppiumOption("appium:appActivity", Configuration.AppActivity);
+            options.AddAdditionalAppiumOption("appium:adbExexTimeout", 600000);
+            
+            return new AndroidDriver(new Uri(Configuration.AppiumServer), options);
+        }
+    }
+}
