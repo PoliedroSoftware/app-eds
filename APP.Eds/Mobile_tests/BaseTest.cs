c8c31c73f4017cc7109614cc88a1074decdec699
William0814 <king-master90@hotmail.com>
2025-07-23 22:59:38 +0200

    Add Test AdminFrame

---
+++ APP.Eds/Mobile_tests/BaseTest.cs
@@ -0,0 +1,29 @@
+﻿using Helpers.Drivers.Mobile;
+using Helpers.Interfaces;
+using Mobile_views;
+
+namespace Mobile_tests;
+
+public class BaseTest
+{
+    protected IDriverManager _driver;
+    [OneTimeSetUp]
+    public void GlobalSetup()
+    {
+        _driver = new MobileDriverManager();
+        Thread.Sleep(3000);
+
+        var loginView = new LoginView(_driver);
+        loginView.Username.SetText("admin");
+        loginView.Password.SetText("admin");
+        loginView.LoginButton.Click();
+
+        Thread.Sleep(3000);
+    }
+
+    [OneTimeTearDown]
+    public void GlobalTearDown()
+    {
+        _driver.Close();
+    }
+}
