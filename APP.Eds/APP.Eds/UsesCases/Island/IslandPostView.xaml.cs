e62e135dd8e5564b223c2ebd544e58f898b05ae2
JOSE NAVARRO <janavarro724@gmail.com>
2025-08-25 14:57:54 -0500

    feat(Island) re desing add Island

--- APP.Eds/APP.Eds/UsesCases/Island/IslandPostView.xaml.cs
+++ APP.Eds/APP.Eds/UsesCases/Island/IslandPostView.xaml.cs
@@ -5,11 +5,35 @@ namespace APP.Eds.UsesCases.Island;
 public partial class IslandPostView : ContentPage
 {
     private IslandService _islandService;
+    
     public IslandPostView()
     {
         InitializeComponent();
         _islandService = new IslandService();
         BindingContext = _islandService;
+    }
+
+    private async void OnBackButtonClicked(object sender, EventArgs e)
+    {
+        try
+        {
+            await Navigation.PopAsync();
+        }
+        catch (Exception ex)
+        {
+            await DisplayAlert("Error", $"Error al navegar: {ex.Message}", "OK");
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        
+        // Refresh islands list when page appears
+        if (_islandService != null)
+        {
+            await _islandService.GetIslandAsync();
+        }
     }
 
     private async void Button_Clicked_1(object sender, EventArgs e)
