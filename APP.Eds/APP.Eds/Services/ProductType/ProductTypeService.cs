a00017cc5af4ebe7beaab477ae3ad5bbfa5a04fd
Erika <johannita.1125@gmail.com>
2025-08-25 11:16:35 -0500

    listing setting

--- APP.Eds/APP.Eds/Services/ProductType/ProductTypeService.cs
+++ APP.Eds/APP.Eds/Services/ProductType/ProductTypeService.cs
@@ -1,3 +1,4 @@
+﻿
 ﻿using APP.Eds.Models.ProductType;
 using System.ComponentModel;
 using System.Text.Json;
@@ -6,6 +7,7 @@ using System.Windows.Input;
 using APP.Eds.Services.Config;
 using APP.Eds.Helpers;
 using System.Net.Http.Headers;
+using System.Collections.ObjectModel;
 
 namespace APP.Eds.Services.ProductType
 {
@@ -17,15 +19,41 @@ namespace APP.Eds.Services.ProductType
         private string? _authToken;
         public ProductTypeModel ProductType
         {
-                get => _productType;
-                set
-                {
+            get => _productType;
+            set
+            {
                 _productType = value;
                 OnPropertyChanged(nameof(ProductType));
+        private ObservableCollection<ProductTypeModel> _productTypeList;
+        public ObservableCollection<ProductTypeModel> ProductTypeList
+        {
+            get => _productTypeList;
+            set
+            {
+                _productTypeList = value;
+                OnPropertyChanged(nameof(ProductTypeList));
             }
         }
 
-       
+        private ObservableCollection<ProductTypeModel> _product
+            }
+        }
+
+        public ICommand EditProductTypeCommand { get; }
+        public ICommand DeleteProductTypeCommand { get; }
+        private ObservableCollection<ProductTypeModel> _productTypeList;
+        public ObservableCollection<ProductTypeModel> ProductTypeList
+        {
+            get => _productTypeList;
+
+            EditProductTypeCommand = new Command<ProductTypeModel
+            set
+            {
+                _productTypeList = value;
+                OnPropertyChanged(nameof(ProductTypeList));
+            }
+        }
+
         private string _description;
         public string Description
         {
@@ -36,86 +64,4 @@ namespace APP.Eds.Services.ProductType
                 OnPropertyChanged(nameof(Description));
             }
         }
-        public ICommand GetByIdProductTypeDataCommand { get; }
-        public ICommand SaveProductTypeDataCommand { get; }
-
-        public ProductTypeService()
-        {
-            GetByIdProductTypeDataCommand = new Command<int>(async (productTypeId) => await GetByIdProductTypeDataAsync(productTypeId));
-            SaveProductTypeDataCommand = new Command(async () => await SaveProductTypeDataAsync());
-            _authToken = TokenHelper.LoadToken(Configuration.KeycloakCliendId, Configuration.KeycloakRealms);
-        }
-
-        public async Task GetByIdProductTypeDataAsync(int productTypeId)
-        {
-            if (string.IsNullOrEmpty(_authToken))
-            {
-                await Application.Current.MainPage.DisplayAlert("Error", "No se encontró el token de autenticación", "OK");
-                return;
-            }
-            try
-            {
-                using var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _authToken);
-                var response = await httpClient.GetStringAsync($"{Configuration.BaseUrl}/api/v1/producttype{ productTypeId}");
-                Console.WriteLine(response);
-
-                ProductType = JsonSerializer.Deserialize<ProductTypeModel>(response, new JsonSerializerOptions
-                {
-                    PropertyNamingPolicy = JsonNamingPolicy.CamelCase
-                });
-            }
-            catch (Exception ex)
-            {
-                await Application.Current.MainPage.DisplayAlert("Error", $"No se pudo cargar el dato: {ex.Message}", "OK");
-            }
-        }
-
-        public async Task SaveProductTypeDataAsync()
-        {
-            if (string.IsNullOrEmpty(_authToken))
-            {
-                await Application.Current.MainPage.DisplayAlert("Error", "No se encontró el token de autenticación", "OK");
-                return;
-            }
-            try
-            {
-                ProductType = new ProductTypeModel
-                {
-                    Description = Description
-                };
-
-                Request = new ProductTypeRequest
-                {
-                    Request = ProductType
-                };
-
-                using var httpClient = new HttpClient();
-                httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", _authToken);
-                var json = JsonSerializer.Serialize(Request, new JsonSerializerOptions { PropertyNamingPolicy = JsonNamingPolicy.CamelCase });
-                var content = new StringContent(json, Encoding.UTF8, "application/json");
-                var response = await httpClient.PostAsync($"{Configuration.BaseUrl}/api/v1/producttype", content);
-
-                if (response.IsSuccessStatusCode)
-                {
-                    await Application.Current.MainPage.DisplayAlert("Éxito", "Datos enviados correctamente", "OK");
-                }
-                else
-                {
-                    var error = await response.Content.ReadAsStringAsync();
-                    await Application.Current.MainPage.DisplayAlert("Error", $"No se pudo enviar el dato: {response.StatusCode}\n{error}", "OK");
-                }
-            }
-            catch (Exception ex)
-            {
-                await Application.Current.MainPage.DisplayAlert("Error", $"Error al enviar los datos: {ex.Message}", "OK");
-            }
-        }
-
-
-        protected void OnPropertyChanged(string propertyName)
-        {
-            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-        }
-    }
-}
+        public ICommand GetByIdProductTypeData
