949f28e34462411fb3618325bae8496eee925a90
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-08-27 11:53:18 -0500

    feat(login) re desing EdsTankPostView

---
+++ APP.Eds/APP.Eds/Converters/ValidationConverters.cs
@@ -0,0 +1,32 @@
+using System.Globalization;
+
+namespace APP.Eds.Converters
+{
+    public class IsNotNullConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class IsNotNullOrEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string str)
+                return !string.IsNullOrEmpty(str);
+            return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
