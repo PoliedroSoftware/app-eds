f042690fa6a6f029f5d0821686ad7aba58bedee4
Eduar Sanchez <leosanchez_19@hotmail.com>
2025-09-03 11:31:20 -0500

    style(court): add style list CourtDetailPopup

--- APP.Eds/APP.Eds/Converters/ValidationConverters.cs
+++ APP.Eds/APP.Eds/Converters/ValidationConverters.cs
@@ -1,4 +1,5 @@
 using System.Globalization;
+using System.Collections;
 
 namespace APP.Eds.Converters
 {
@@ -22,6 +23,36 @@ namespace APP.Eds.Converters
             if (value is string str)
                 return !string.IsNullOrEmpty(str);
             return value != null;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class CountToBoolConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is ICollection collection)
+                return collection.Count > 0;
+            if (value is int count)
+                return count > 0;
+            return false;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class StringNotEmptyConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return !string.IsNullOrWhiteSpace(value?.ToString());
         }
 
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
