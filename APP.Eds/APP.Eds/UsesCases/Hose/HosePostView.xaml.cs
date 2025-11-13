3b98febed1d03c33b7308827ac8a2cc1ea342a29
Erika Ardila <Johannita.1125@gmail.com>
2025-11-13 15:31:13 -0500

    Hu 271 orange buton (#368)
    * Orange buton
    
    * botton
    
    * botton
    
    * botton orange
    
    * botton
    
    * botton
    
    * botton
    
    * botton
    
    * botton
    
    * botton
    
    ---------
    
    Co-authored-by: Eduar Sanchez <leosanchez_19@hotmail.com>

--- APP.Eds/APP.Eds/UsesCases/Hose/HosePostView.xaml.cs
+++ APP.Eds/APP.Eds/UsesCases/Hose/HosePostView.xaml.cs
@@ -16,6 +16,16 @@ public partial class HosePostView : ContentPage
         BindingContext = _hoseService;
     }
 
+     protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        // Llamada correcta al m�todo GetHoseAsync del servicio
+        await _hoseService.GetHoseAsync();
+
+    }
+
+
     private async void OnCompartimentButtonClicked(object sender, EventArgs e)
     {
         await Navigation.PushAsync(new CompartimentPostView());
@@ -122,8 +132,8 @@ public partial class HosePostView : ContentPage
                 Number = 0;
                 AccumulatedAmount = 0;
                 AccumulatedGallons = 0;
-                _hoseService.SelectedDispensers = null;
-                _hoseService.SelectProductType = null;
+                _hoseService.SelectedDispensers = default!;
+                _hoseService.SelectProductType = default!;
             }
             catch (Exception ex)
             {
